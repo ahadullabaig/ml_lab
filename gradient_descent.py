49c78a774d7d3c53555418070bc244c8d33cad71
ahadullabaig <ahadullabaig.16@gmail.com>
2026-05-19 00:10:08 +0530

    added graph plotting for gradient descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("datasets/gb.csv")
+f = pd.read_csv("datasets/gb.csv")
 
-x = df.iloc[:, 0].values
-y = df.iloc[:, 1].values
+x = f.iloc[:, 0].values
+y = f.iloc[:, 1].values
 
 m, c = 0, 0
+
 lr, epochs = 0.0001, 10000
 
 n = len(x)
 
 for _ in range(epochs):
-    y_pred = m * x + c
+    y_pred = m*x + c
 
     dm = (-2 / n) * sum(x * (y - y_pred))
     dc = (-2 / n) * sum(y - y_pred)
@@ -22,7 +24,11 @@ for _ in range(epochs):
 print(f"Slope (m): {m:.4f}")
 print(f"Intercept (c): {c:.4f}")
 
-x_new = float(input("Enter a value for x: "))
-y_new = m * x_new + c
+plt.scatter(x, y)
 
-print(f"Predicted y: {y_new:.4f}")
+plt.plot(x, m*x+c)
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
